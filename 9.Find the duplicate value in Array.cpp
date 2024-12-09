97c5a4c906542b95747a173b68e7dec8192051b7
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-09 18:02:12 +0530

    update

--- 9.Find the duplicate value in Array.cpp
+++ 9.Find the duplicate value in Array.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 int main()
 {
-   int arr[]={1,2,1,3,4,1};
+   int arr[]={1,2,1,3,2};
 
-   for(int i=0;i<=6;++i)
+   for(int i=0;i<5;++i)
    {
-  
-      for(int j=i+1;j<=6;++j)
-   {
-    if(arr[i]==arr[j])
-    {
-       cout<<"Duplicate value is="<<arr[i];
-       arr[j]=-1;
-       break;
-    }
-   }
+       bool dup=false;
+      for(int j=i+1;j<5;++j)
+      {
+         if(arr[i]==arr[j] && arr[j]!=-1)
+         {
+            dup=true;
+            arr[j]=-1;
+            break;
+         }
+     }
+     if(dup && arr[i]!=-1)
+     {
+      cout<<arr[i];
+     }
 }
 }
